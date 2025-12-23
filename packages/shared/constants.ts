213b45a97eb5daa55448671d8188115067c11b3c
Nikhil Dhiman <nikhil.x.dhiman@gmail.com>
2025-12-23 09:08:32 +0530

    1. Initialize pnpm 2. Setup pnpm Workspace 3. Basic Folder Structure

---
+++ packages/shared/constants.ts
@@ -0,0 +1,8 @@
+export const SOCKET_EVENTS = {
+  CONNECTION: 'connection',
+  GREETING: 'greeting',
+  SEND_MESSAGE: 'send-message',
+  MESSAGE_DELIVERED: 'message-delivered',
+  MESSAGE_SEEN: 'message-seen',
+  DISCONNECT: 'disconnect',
+} as const;
